c9eb022e94c7da9cf0723ffda76cd574bcb659bb
Hamoud Alhoqbani <h.alhoqbani@gmail.com>
2017-06-04 00:20:52 -0700

    Refactor Stripe test

--- tests/unit/Billing/StripePaymentGatewayTest.php
+++ tests/unit/Billing/StripePaymentGatewayTest.php
@@ -6,25 +6,54 @@ use App\Billing\StripePaymentGateway;
 class StripePaymentGatewayTest extends TestCase
 {
     
-    /** @test */
-    public function charges_with_a_valid_payment_token_are_successful()
+    private $lastCharge;
+    
+    protected function setUp()
     {
-        $paymentGateway = new StripePaymentGateway(config('services.stripe.key'));
-        $token = \Stripe\Token::create([
-            "card" => [
-                "number"    => "4242424242424242",
-                "exp_month" => 1,
-                "exp_year"  => date('Y') + 1,
-                "cvc"       => "123",
-            ],
-        ], ["api_key" => config('services.stripe.key')])->id;
-        $paymentGateway->charge(2500, $token);
-        
-        $lastCharge = \Stripe\Charge::all(
-            ["limit" => 3],
-            ["api_key" => config('services.stripe.key')]
-        )['data'][0];
-        
-        $this->assertEquals(2500, $lastCharge->amount);
+        parent::setUp();
+        $this->lastCharge = $this->lastCharge();
     }
+    
+    private function lastCharge()
+    {
+        return array_first(\Stripe\Charge::all(
+            ['limit' => 1],
+            ['api_key' => config('services.stripe.secret')]
+        )['data']);
+    }
+    
+    private function newCharges()
+    {
+        return \Stripe\Charge::all(
+            [
+                'ending_before' => $this->lastCharge ? $this->lastCharge->id : null,
+            ],
+            ['api_key' => config('services.stripe.secret')]
+        )['data'];
+    }
+    
+    /** @test */
+    function charges_with_a_valid_payment_token_are_successful()
+    {
+        $paymentGateway = new StripePaymentGateway(config('services.stripe.secret'));
+        $paymentGateway->charge(2500, $this->validToken());
+        $this->assertCount(1, $this->newCharges());
+        $this->assertEquals(2500, $this->lastCharge()->amount);
+    }
+    
+    /**
+     * @return string
+     */
+    private function validToken()
+    {
+        return \Stripe\Token::create([
+            "card" => [
+                "number" => "4242424242424242",
+                "exp_month" => 1,
+                "exp_year" => date('Y') + 1,
+                "cvc" => "123"
+            ]
+        ], ['api_key' => config('services.stripe.secret')])->id;
+    }
+
 }
