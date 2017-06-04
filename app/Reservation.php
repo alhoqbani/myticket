7b0199eefc4c32efd63267ae7980f6c06319bdf7
Hamoud Alhoqbani <h.alhoqbani@gmail.com>
2017-06-03 21:18:55 -0700

    Refacotr mocking

--- app/Reservation.php
+++ app/Reservation.php
@@ -6,10 +6,17 @@ use Illuminate\Support\Collection;
 
 class Reservation
 {
-    //
     public function __construct(Collection $tickets)
     {
         $this->tickets = $tickets;
+    }
+    
+    public function cancel()
+    {
+        /** @var \App\Ticket $ticket */
+        foreach ($this->tickets as $ticket) {
+            $ticket->release();
+        }
     }
     
     public function totalCost()
--- tests/unit/ReservationTest.php
+++ tests/unit/ReservationTest.php
@@ -1,20 +1,40 @@
 <?php
 
-use App\Concert;
 use App\Reservation;
+use App\Ticket;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 
 class ReservationTest extends TestCase
 {
+    
     /** @test */
     public function calculating_the_total_cost()
     {
         $reservation = new Reservation(collect([
-            (object) ['price' => 1200],
-            (object) ['price' => 1200],
-            (object) ['price' => 1200],
+            (object)['price' => 1200],
+            (object)['price' => 1200],
+            (object)['price' => 1200],
         ]));
         $this->assertEquals(3600, $reservation->totalCost());
+    }
     
+    /** @test */
+    public function reserved_tickets_are_released_when_a_reservation_is_canceled()
+    {
+        $tickets = collect([
+            Mockery::mock(Ticket::class, function ($mock) {
+                $mock->shouldReceive('release')->once();
+            }),
+            Mockery::mock(Ticket::class, function ($mock) {
+                $mock->shouldReceive('release')->once();
+            }),
+            Mockery::mock(Ticket::class, function ($mock) {
+                $mock->shouldReceive('release')->once();
+            }),
+        ]);
+        
+        $reservation = new Reservation($tickets);
+        
+        $reservation->cancel();
     }
 }
